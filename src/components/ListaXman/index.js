f3943ee63d10884dedc23a7bea70664efed355db
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2022-02-13 18:27:45 -0800

    Update index.js

--- src/components/ListaXman/index.js
+++ src/components/ListaXman/index.js
@@ -3,17 +3,54 @@ import { listaHeroi } from "../../api/api";
 import appConfig from "../../config.json"
 import styled from "styled-components";
 
-
-const Div = styled.div`
-    display: flex;
-    
-`;
+const Lista = styled.div`
+    border: 1px outset #000000;
+    margin-left: 5px;
+    margin-bottom: 10px;
+    width: 100%;
+`
 const H1 = styled.h1`
     padding: 0;
     margin: 0;
     margin-left: 2.5rem;
 `
+const CaixaCard = styled.div`
+    border: 1px outset #000000;
+    margin: 7px;
+    position: relative;
+    display: block;
+    transition: transform 500ms;
+    flex: 1 1 0px;   
+    &:hover{
+        transform: translateX(-25%);
+    }
+    &:hover ~ &, &:focus ~ &{
+        transform: translateX(25%);      
+    }
+    
+`
 
+const Div = styled.div`
+    display: flex;
+    & ${CaixaCard}:hover, &:focus ${CaixaCard} {
+        transform: scale(1.5);
+        z-index: 1;
+        cursor: pointer;
+    }
+    &:hover ${CaixaCard}, &:focus-within ${CaixaCard} {
+      //  transform: translateX(-25%);
+    }
+    
+    
+`;
+const CardSecundario = styled.figure`
+    margin: 10px;
+    padding: 0;
+    
+`
+const Figcaption = styled.figcaption`
+    text-align: center;
+`
 
 export default () => {
     const XmanNumeros = [];
@@ -31,29 +68,30 @@ export default () => {
             listaHeroi(num);
         })
         console.log('setando o loading')
-        setIsLoading(true)        
+        setIsLoading(true)
 
     }, []);
 
     return (
         <>
             {console.log(appConfig.xman)}
-            <div>
+            <Lista>
                 <H1>X-Men</H1>
                 <Div>
 
                     {appConfig.xman.map(heroi => {
                         return (
-
-                            <figure key={heroi.name}>
-                                <img src={heroi.img} alt="Imagem de um integrante Xman"></img>
-                                <figcaption>{heroi.name}</figcaption>
-                            </figure>
+                            <CaixaCard>
+                                <CardSecundario key={heroi.name}>
+                                    <img src={heroi.img} alt="Imagem de um integrante Xman"></img>
+                                    <Figcaption>{heroi.name}</Figcaption>
+                                </CardSecundario>
+                            </CaixaCard>
 
                         )
                     })}
                 </Div>
-            </div>
+            </Lista>
 
         </>
     );
