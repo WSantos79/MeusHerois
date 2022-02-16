b93d6ac16c136e5702942236ff5dae85d4596e0a
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2022-02-15 17:58:32 -0800

    começando barra funcional

--- src/components/ListaXman/index.js
+++ src/components/ListaXman/index.js
@@ -3,14 +3,21 @@ import { listaHeroi } from "../../api/api";
 import appConfig from "../../config.json"
 import styled from "styled-components";
 
+
+const Area = styled.div`
+    display:flex;
+    padding: 0;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 5rem;
+`
+
 const Lista = styled.div`
     position: relative;
-    margin: auto;
-   
-
+   // margin: auto;
+    overflow: hidden;
 
     border: 1px outset #000000;
-    margin-bottom: 5rem;
     width: 80%;
 `
 const H1 = styled.h1`
@@ -18,21 +25,27 @@ const H1 = styled.h1`
     margin: 0;
     margin-left: 2.5rem;
 `
+const BtnLeft = styled.button`
+    width: 30px;
+    height: 2rem;
+`
+const BtnRight = styled.button`
+    width: 30px;
+    height: 2rem;
+`
 
 const CaixaCard = styled.div`
     border: 1px outset #000000;
     margin: 7px;
     transition: transform 500ms;
     &:hover ~ &, &:focus ~ &{
-        transform: translateX(25%);      
-    }    
+        transform: translateX(25%);   
+    }
+    
 `
-const Div = styled.div`
+const Div = styled.div`    
     display: flex;
-   // overflow-x: scroll;
-   // overflow-y: hidden;
-   // white-space: wrap;
-
+   
     & ${CaixaCard}:hover, &:focus ${CaixaCard} {
         transform: scale(1.5) ;
         z-index: 1;
@@ -56,6 +69,24 @@ export default () => {
     const XmanNumeros = [];
     const [isLoading, setIsLoading] = useState(false);
     const firstUpdate = useRef(true);
+    const [scroll, setScroll] = useState(0);
+
+    function handleRight() {
+        let x = scroll - Math.round(window.innerWidth / 2);
+        let listW = appConfig.xman.length * 150;
+        if (window.innerWidth - listW > x) {
+            x = (window.innerWidth - listW) - 60;
+        }
+        setScroll(x);
+    }
+
+    function handleLeft() {
+        let x = scroll + Math.round(window.innerWidth / 2);
+        if(x > 0){
+            x = 0;
+        }
+        setScroll(x);
+    }
 
     useEffect(() => {
         if (firstUpdate.current) {
@@ -73,25 +104,35 @@ export default () => {
     }, []);
 
     return (
-        <>
-            {console.log(appConfig.xman)}
-            <Lista>
-                <H1>X-Men</H1>
-                <Div>
+        <>            
+            <Area>
+                <BtnLeft onClick={() => {
+                    handleLeft();
+                }}/>
+                <Lista>
+                    <H1>X-Men</H1>
+                    <Div style={{
+                    marginLeft: scroll,
+                    width: appConfig.xman.length * 150
+                }}>
+                       
+                        {appConfig.xman.map(heroi => {
+                            return (
+                                <CaixaCard>
+                                    <CardSecundario key={heroi.name}>
+                                        <img src={heroi.img} alt="Imagem de um integrante Xman"></img>
+                                        <Figcaption>{heroi.name}</Figcaption>
+                                    </CardSecundario>
+                                </CaixaCard>
 
-                    {appConfig.xman.map(heroi => {
-                        return (
-                            <CaixaCard>
-                                <CardSecundario key={heroi.name}>
-                                    <img src={heroi.img} alt="Imagem de um integrante Xman"></img>
-                                    <Figcaption>{heroi.name}</Figcaption>
-                                </CardSecundario>
-                            </CaixaCard>
-
-                        )
-                    })}
-                </Div>
-            </Lista>
+                            )
+                        })}
+                    </Div>
+                </Lista>
+                <BtnRight onClick={() => {
+                    handleRight();
+                }} />
+            </Area>
 
         </>
     );
