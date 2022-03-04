baecef9df37b87e3c14adcefb7184eeb9dc11b5c
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2022-03-03 17:29:18 -0800

    Update GlobalStyle.js

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -2,16 +2,21 @@ import { createGlobalStyle } from "styled-components";
 import {corPrimaria, corTexto} from "./UI/variaveis"
 
 export const GlobalStyle = createGlobalStyle`
-	
-body {
-	background: ${corPrimaria};
-	color: ${corTexto};
+
+* {
 
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap');
   font-family: 'Source Sans Pro', sans-serif;
 
   margin: 0;
   padding: 0;
+  outline: 0;
   box-sizing: border-box;
+}	
+
+body {
+	background: ${corPrimaria};
+	color: ${corTexto};
 }
+
 `
